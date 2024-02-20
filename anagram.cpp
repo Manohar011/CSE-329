ef860c3a7b2e5a1f23dbc66468d9ad2ae60223f4
Manohar011 <114577262+Manohar011@users.noreply.github.com>
2024-02-20 22:32:38 +0530

    Add files via upload

---
+++ anagram.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void quick(int arr[],)
+main()
+{
+	string a = "listen";
+	string b="silent";
+	
+	int i= a.length();
+	int j= b.length();
+	
+	for(int k=0;k<i;k++)
+	{
+		if(a==b)
+		{
+			cout<<"anagram";
+		}else
+		cout<<"as";
+	}
+	
+}
